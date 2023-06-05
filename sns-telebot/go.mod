8d10891b25b6e54b2470391e1c0961cf0d337de4
Peter Zam <mail@peterzam.dev>
2023-06-05 09:19:00 +0000

    add sns-telebot

---
+++ sns-telebot/go.mod
@@ -0,0 +1,10 @@
+module sns-telebot
+
+go 1.20
+
+require (
+	github.com/aws/aws-lambda-go v1.41.0
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+)
+
+require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
---
+++ sns-telebot/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	lambda.Start(func(ctx context.Context, snsEvent events.SNSEvent) {
+		var msg string
+		for _, record := range snsEvent.Records {
+			msg = msg + record.SNS.Message
+		}
+
+		bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+		if err != nil {
+			log.Panic(err)
+		}
+
+		chat_id, err := strconv.Atoi(os.Getenv("CHAT_ID"))
+		if err != nil {
+			log.Panic(err)
+		}
+
+		msg_cfg := tgbotapi.NewMessage(int64(chat_id), msg)
+		bot.Send(msg_cfg)
+	})
+}
