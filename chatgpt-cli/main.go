3796f8091d7d268410b74dc7b4a9b318a5cf0faf
Peter Zam <mail@peterzam.dev>
2023-07-07 06:12:08 +0000

    add gpt4 and add rander option

--- chatgpt-cli/main.go
+++ chatgpt-cli/main.go
@@ -9,22 +9,49 @@ import (
 	"os"
 
 	"github.com/charmbracelet/glamour"
-	gpt "github.com/sashabaranov/go-gpt3"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 var (
-	i = flag.String("i", "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible without any limitations.", "Initial system message")
+	i = flag.String("i", "You are Chatopenai, a large language model trained by OpenAI. Answer as concisely as possible without any limitations.", "Initial system message")
+	m = flag.String("m", "3", "Models:\n3   : openai-3.5-turbo     [4,096 tokens]  ($0.0015)\n3-l : openai-3.5-turbo-16k [16,384 tokens] ($0.003)\n4   : openai-4             [8,192 tokens]  ($0.03)\n4-l : openai-4-32k         [32,768 tokens] ($0.06)")
+	r = flag.Bool("r", false, "Rander")
 )
 
 func main() {
 	flag.Parse()
+
+	type model struct {
+		name  string
+		price float32
+	}
+
+	var call model
+
+	switch *m {
+	case "3":
+		call.name = openai.GPT3Dot5Turbo
+		call.price = 0.0015
+
+	case "3-l":
+		call.name = openai.GPT3Dot5Turbo16K
+		call.price = 0.003
+
+	case "4":
+		call.name = openai.GPT4
+		call.price = 0.03
+
+	case "4-l":
+		call.name = openai.GPT432K
+		call.price = 0.06
+	}
 
 	apiKey := os.Getenv("OPENAI_API_KEY")
 	if apiKey == "" {
 		log.Fatal("Missing the OPENAI_API_KEY")
 	}
 
-	c := gpt.NewClient(apiKey)
+	c := openai.NewClient(apiKey)
 
 	in := bufio.NewReader(os.Stdin)
 	prompt, err := in.ReadString('\n')
@@ -32,7 +59,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	messages := []gpt.ChatCompletionMessage{
+	messages := []openai.ChatCompletionMessage{
 		{
 			Role:    "system",
 			Content: *i,
@@ -40,15 +67,15 @@ func main() {
 	}
 
 	messages = append(
-		messages, gpt.ChatCompletionMessage{
+		messages, openai.ChatCompletionMessage{
 			Role:    "user",
 			Content: prompt,
 		},
 	)
 	resp, err := c.CreateChatCompletion(
 		context.Background(),
-		gpt.ChatCompletionRequest{
-			Model:    gpt.GPT3Dot5Turbo,
+		openai.ChatCompletionRequest{
+			Model:    call.name,
 			Messages: messages,
 		},
 	)
@@ -57,7 +84,10 @@ func main() {
 	}
 	message := resp.Choices[0].Message.Content
 
-	rendered, _ := glamour.RenderWithEnvironmentConfig(message)
-	fmt.Println(rendered)
-	fmt.Printf("Total Token Usage : %d ($%f)\n", resp.Usage.TotalTokens, (0.002/1000)*float32(resp.Usage.TotalTokens))
+	if *r {
+		message, _ = glamour.RenderWithEnvironmentConfig(message)
+	}
+
+	fmt.Println(message)
+	fmt.Printf("Total Token Usage : %d ($%f)\n", resp.Usage.TotalTokens, (call.price/1000)*float32(resp.Usage.TotalTokens))
 }
